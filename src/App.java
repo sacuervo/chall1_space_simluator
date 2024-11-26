24719cf529387d3ac0fd5bc7b44a2ccd180cdbba
Santiago Cuervo <cuervo.santiago@gmail.com>
2024-11-26 09:22:04 -0500

    Define basic app funcitonality

---
+++ src/App.java
@@ -0,0 +1,31 @@
+
+// TODO:ALMACENAR INFORMACIÓN
+
+// TODO:IMPLEMENTAR MENÚ PRINCIPAL:
+// TODO:Opción 1:Seleccionar planeta 
+// TODO:Opción 2:Elegir una nave 
+// TODO:Opción 3:Iniciar viaje 
+// TODO:Opción 4:Salir del programa
+
+// TODO:IMPLEMENTAR SELECCIÓN DE PLANETA:
+// TODO:Mostrar distancia 
+// TODO:Mostrar breve descripción del planeta
+
+// TODO:ADMINISTRAR RECURSOS DE LA NAVE
+
+// TODO:IMPLEMENTAR EVENTOS ALEATORIOS EN EL VIAJE 
+// TODO:Mostrar descripción del evento 
+// TODO:Darle opciones al usuario de cómo afrontarlo
+
+// TODO:IMPLEMENTAR SIMULACIÓN DEL VIAJE 
+// TODO:Calcular duración del viaje estimada en días 
+// TODO:Calcular progreso del viaje mediante bucles básicos 
+
+// TODO:Mostrar etapas clave del viaje(inicio,mitad y llegada al destino)
+// TODO:Generar un número aleatorio y contrastarlo con un una condición que signifique la ocurrencia de un evento inesperado
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
